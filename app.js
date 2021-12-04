9269009048fe9dc48726b3f95bb4b714f7bb06a3
epmitdev-Rai <adwitya.paramaartha@enseval.com>
2021-12-05 01:38:46 +0700

    parse to detailed data using 2d array

--- app.js
+++ app.js
@@ -3,29 +3,6 @@ exports.__esModule = true;
 var export_to_csv_1 = require("export-to-csv");
 var fs = require("fs");
 var csvParse = require("csv-parse");
-//var data = [
-//    {
-//        name: 'Test 1',
-//        age: 13,
-//        average: 8.2,
-//        approved: true,
-//        description: "using 'Content hereeee, content here' "
-//    },
-//    {
-//        name: 'Test 2',
-//        age: 11,
-//        average: 8.2,
-//        approved: true,
-//        description: "using 'Content here, content here' "
-//    },
-//    {
-//        name: 'Test 4',
-//        age: 10,
-//        average: 8.2,
-//        approved: true,
-//        description: "using 'Content here, content here' "
-//    },
-//];
 var options = {
     fieldSeparator: ',',
     quoteStrings: '"',
@@ -34,22 +11,27 @@ var options = {
     showTitle: false,
     useTextFile: false,
     useBom: true,
-    useKeysAsHeaders: true
+    useKeysAsHeaders: false
 };
 var exportToCsv = new export_to_csv_1.ExportToCsv(options);
 var parserSource = csvParse.parse({ delimiter: ';' }, function (err, data) {
     var csvDataSource = exportToCsv.generateCsv(data, true);
     fs.writeFileSync(__dirname + '/csv_Files/Outputs/fileSource.txt', csvDataSource);
-    console.log(data);
+    //console.log(data);
 });
 var parserProxy = csvParse.parse({ delimiter: ';' }, function (err, data) {
     var csvDataProxy = exportToCsv.generateCsv(data, true);
     fs.writeFileSync(__dirname + '/csv_Files/Outputs/fileProxy.txt', csvDataProxy);
-    console.log(data);
+    //  console.log(data);
 });
+var dataObject = fs.readFileSync(__dirname + '/csv_Files/Inputs/proxy.csv', {
+    encoding: 'utf8'
+}).split('\n').map(function (row) {
+    return row.split(',');
+});
+console.log(dataObject);
+console.log(dataObject[0][1]);
+console.log(dataObject[0][2]);
 fs.createReadStream(__dirname + '/csv_Files/Inputs/proxy.csv').pipe(parserProxy);
 fs.createReadStream(__dirname + '/csv_Files/Inputs/source.csv').pipe(parserSource);
-//const csvExporter = new ExportToCsv(options);
-//const csvData = exportToCsv.generateCsv(data, true);
 console.log('done');
-//csvExporter.generateCsv(data);
--- app.ts
+++ app.ts
@@ -3,29 +3,6 @@ import { ExportToCsv } from 'export-to-csv';
 import * as fs from 'fs';
 import * as csvParse from 'csv-parse';
 
-//var data = [
-//    {
-//        name: 'Test 1',
-//        age: 13,
-//        average: 8.2,
-//        approved: true,
-//        description: "using 'Content hereeee, content here' "
-//    },
-//    {
-//        name: 'Test 2',
-//        age: 11,
-//        average: 8.2,
-//        approved: true,
-//        description: "using 'Content here, content here' "
-//    },
-//    {
-//        name: 'Test 4',
-//        age: 10,
-//        average: 8.2,
-//        approved: true,
-//        description: "using 'Content here, content here' "
-//    },
-//];
 const options = {
     fieldSeparator: ',',
     quoteStrings: '"',
@@ -34,21 +11,33 @@ const options = {
     showTitle: false,
     useTextFile: false,
     useBom: true,
-    useKeysAsHeaders: true,
+    useKeysAsHeaders: false,
     // headers: ['Column 1', 'Column 2', etc...] <-- Won't work with useKeysAsHeaders present!
 };
 const exportToCsv = new ExportToCsv(options);
 var parserSource = csvParse.parse({ delimiter: ';' }, function (err, data) {
     const csvDataSource = exportToCsv.generateCsv(data, true);
     fs.writeFileSync(__dirname + '/csv_Files/Outputs/fileSource.txt', csvDataSource)
-    console.log(data);
+//console.log(data);
 });
 
 var parserProxy = csvParse.parse({ delimiter: ';' }, function (err, data) {
     const csvDataProxy = exportToCsv.generateCsv(data, true);
     fs.writeFileSync(__dirname + '/csv_Files/Outputs/fileProxy.txt', csvDataProxy)
-    console.log(data);
+  //  console.log(data);
 });
+
+
+const dataObject = fs.readFileSync(__dirname + '/csv_Files/Inputs/proxy.csv', {
+    encoding: 'utf8'
+}).split('\n').map((row: string): string[] => {
+    return row.split(',');
+})
+
+console.log(dataObject);
+console.log(dataObject[0][1]); // [row][col]
+console.log(dataObject[0][2]);
+
 
 fs.createReadStream(__dirname + '/csv_Files/Inputs/proxy.csv').pipe(parserProxy);
 fs.createReadStream(__dirname + '/csv_Files/Inputs/source.csv').pipe(parserSource);
