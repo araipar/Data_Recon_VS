c82ef875635e4c6767f412ab25b4004a8fd3ded5
epmitdev-Rai <adwitya.paramaartha@enseval.com>
2021-12-05 00:46:25 +0700

    revised  Csv parse and export

--- app.js
+++ app.js
@@ -1,51 +1,55 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const export_to_csv_1 = require("export-to-csv");
-const fs = require("fs");
-const csvParse = require("csv-parse");
-var data = [
-    {
-        name: 'Test 1',
-        age: 13,
-        average: 8.2,
-        approved: true,
-        description: "using 'Content hereeee, content here' "
-    },
-    {
-        name: 'Test 2',
-        age: 11,
-        average: 8.2,
-        approved: true,
-        description: "using 'Content here, content here' "
-    },
-    {
-        name: 'Test 4',
-        age: 10,
-        average: 8.2,
-        approved: true,
-        description: "using 'Content here, content here' "
-    },
-];
-var parser = csvParse.parse({ delimiter: ';' }, function (err, data) {
-    console.log(data);
-});
-fs.createReadStream(__dirname + '/csv_Files/Outputs/proxy.csv').pipe(parser);
-const options = {
+exports.__esModule = true;
+var export_to_csv_1 = require("export-to-csv");
+var fs = require("fs");
+var csvParse = require("csv-parse");
+//var data = [
+//    {
+//        name: 'Test 1',
+//        age: 13,
+//        average: 8.2,
+//        approved: true,
+//        description: "using 'Content hereeee, content here' "
+//    },
+//    {
+//        name: 'Test 2',
+//        age: 11,
+//        average: 8.2,
+//        approved: true,
+//        description: "using 'Content here, content here' "
+//    },
+//    {
+//        name: 'Test 4',
+//        age: 10,
+//        average: 8.2,
+//        approved: true,
+//        description: "using 'Content here, content here' "
+//    },
+//];
+var options = {
     fieldSeparator: ',',
     quoteStrings: '"',
     decimalSeparator: '.',
     showLabels: true,
     showTitle: false,
-    title: 'My Awesome CSV',
     useTextFile: false,
     useBom: true,
-    useKeysAsHeaders: true,
-    // headers: ['Column 1', 'Column 2', etc...] <-- Won't work with useKeysAsHeaders present!
+    useKeysAsHeaders: true
 };
+var exportToCsv = new export_to_csv_1.ExportToCsv(options);
+var parserSource = csvParse.parse({ delimiter: ';' }, function (err, data) {
+    var csvDataSource = exportToCsv.generateCsv(data, true);
+    fs.writeFileSync(__dirname + '/csv_Files/Outputs/fileSource.txt', csvDataSource);
+    console.log(data);
+});
+var parserProxy = csvParse.parse({ delimiter: ';' }, function (err, data) {
+    var csvDataProxy = exportToCsv.generateCsv(data, true);
+    fs.writeFileSync(__dirname + '/csv_Files/Outputs/fileProxy.txt', csvDataProxy);
+    console.log(data);
+});
+fs.createReadStream(__dirname + '/csv_Files/Inputs/proxy.csv').pipe(parserProxy);
+fs.createReadStream(__dirname + '/csv_Files/Inputs/source.csv').pipe(parserSource);
 //const csvExporter = new ExportToCsv(options);
-const exportToCsv = new export_to_csv_1.ExportToCsv(options);
-const csvData = exportToCsv.generateCsv(data, true);
-fs.writeFileSync(__dirname + '/csv_Files/file.txt', csvData);
+//const csvData = exportToCsv.generateCsv(data, true);
 console.log('done');
 //csvExporter.generateCsv(data);
-//# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -3,53 +3,56 @@ import { ExportToCsv } from 'export-to-csv';
 import * as fs from 'fs';
 import * as csvParse from 'csv-parse';
 
-var data = [
-    {
-        name: 'Test 1',
-        age: 13,
-        average: 8.2,
-        approved: true,
-        description: "using 'Content hereeee, content here' "
-    },
-    {
-        name: 'Test 2',
-        age: 11,
-        average: 8.2,
-        approved: true,
-        description: "using 'Content here, content here' "
-    },
-    {
-        name: 'Test 4',
-        age: 10,
-        average: 8.2,
-        approved: true,
-        description: "using 'Content here, content here' "
-    },
-];
-
-var parser = csvParse.parse({ delimiter: ';' }, function (err, data) {
-    console.log(data);
-});
-
-fs.createReadStream(__dirname + '/csv_Files/Outputs/proxy.csv').pipe(parser);
-
+//var data = [
+//    {
+//        name: 'Test 1',
+//        age: 13,
+//        average: 8.2,
+//        approved: true,
+//        description: "using 'Content hereeee, content here' "
+//    },
+//    {
+//        name: 'Test 2',
+//        age: 11,
+//        average: 8.2,
+//        approved: true,
+//        description: "using 'Content here, content here' "
+//    },
+//    {
+//        name: 'Test 4',
+//        age: 10,
+//        average: 8.2,
+//        approved: true,
+//        description: "using 'Content here, content here' "
+//    },
+//];
 const options = {
     fieldSeparator: ',',
     quoteStrings: '"',
     decimalSeparator: '.',
     showLabels: true,
     showTitle: false,
-    title: 'My Awesome CSV',
     useTextFile: false,
     useBom: true,
     useKeysAsHeaders: true,
     // headers: ['Column 1', 'Column 2', etc...] <-- Won't work with useKeysAsHeaders present!
 };
-
-//const csvExporter = new ExportToCsv(options);
-
 const exportToCsv = new ExportToCsv(options);
-const csvData = exportToCsv.generateCsv(data, true);
-fs.writeFileSync(__dirname + '/csv_Files/file.txt', csvData)
+var parserSource = csvParse.parse({ delimiter: ';' }, function (err, data) {
+    const csvDataSource = exportToCsv.generateCsv(data, true);
+    fs.writeFileSync(__dirname + '/csv_Files/Outputs/fileSource.txt', csvDataSource)
+    console.log(data);
+});
+
+var parserProxy = csvParse.parse({ delimiter: ';' }, function (err, data) {
+    const csvDataProxy = exportToCsv.generateCsv(data, true);
+    fs.writeFileSync(__dirname + '/csv_Files/Outputs/fileProxy.txt', csvDataProxy)
+    console.log(data);
+});
+
+fs.createReadStream(__dirname + '/csv_Files/Inputs/proxy.csv').pipe(parserProxy);
+fs.createReadStream(__dirname + '/csv_Files/Inputs/source.csv').pipe(parserSource);
+
+
+
 console.log('done');
-//csvExporter.generateCsv(data);
